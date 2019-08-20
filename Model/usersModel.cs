3f8f243cdc1b5e14cfd30f188a352ac85b3d7a4b
hubinbing999 <2387108878@qq.com>
2019-08-20 15:37:20 +0800

    第3次

---
+++ Model/usersModel.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class usersModel
+    { 
+                    public System.Int32 u_id { get; set; }
+
+                    
+                    public System.String u_name { get; set; }
+
+                    
+                    public System.String u_true_name { get; set; }
+
+                    
+                    public System.String u_password { get; set; }
+
+                     } }
