0b6a5c5155914f670f136bf6e8aa800fc089cb5f
LENOVO <LENOVO@DESKTOP-TQTSG8V>
2019-08-20 08:46:42 +0800

    第2次

--- UI/Global.asax.cs
+++ UI/Global.asax.cs
@@ -16,6 +16,7 @@ namespace UI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
